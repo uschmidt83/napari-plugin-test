1f294e25bbf3b0c14ebacf3892d23ffd504f431c
Uwe Schmidt <mail@uweschmidt.org>
2022-04-04 13:05:06 +0200

    bump

--- src/napari_plugin_test/__init__.py
+++ src/napari_plugin_test/__init__.py
@@ -1,8 +1,8 @@
 
-__version__ = "0.0.1"
+__version__ = "0.0.3"
 
 
 
 
-from ._sample_data import make_sample_data 
+from ._sample_data import make_sample_data
 from ._widget import ExampleQWidget, example_magic_widget
